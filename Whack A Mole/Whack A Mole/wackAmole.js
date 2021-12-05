24efa49af24858e91db6381a6060de03f88be025
AnbuCap <qidachen808@yahoo.com>
2021-12-05 03:30:58 -0500

    detail in README.md

--- Whack A Mole/Whack A Mole/wackAmole.js
+++ Whack A Mole/Whack A Mole/wackAmole.js
@@ -4,14 +4,27 @@ HERE IS THE JS TO RUN THE GAME
 const holes = document.querySelectorAll(".hole"); // .hole from css
 const scoreBoard = document.querySelector(".score"); // .score from css
 const professors = document.querySelectorAll(".professor"); // .professor from css
+const professors2 = document.querySelectorAll(".professor2");
 const timer = document.querySelector(".time");
+const character = [professors, professors2];
+const holeUp = [];
 let lastHole; // let are variables.
 let timeUp;
 let score;
 let timeCount;
-let timeAmount = 10000;
+let timeAmount = 20000;
 var play = false;
+let hit = new Audio("./audio/Bat_hit.wav");
+let music = new Audio("./audio/GameMusic.zip");
+let ouch = new Audio("./audio/ouch.mp3");
 
+function randChar(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function randAmount(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 //Function to give a random time to show the image
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
@@ -39,14 +52,29 @@ to show in each hole. The numbers are big because it counts in millisecond
 */
 function peep() {
   tick();
+  let hole;
   const time = randomTime(1000, 1000);
-  const hole = randomHole(holes);
-  hole.classList.add("up"); // creating a class list on hole and added up to it. up is in the css.
-  setTimeout(() => {
-    hole.classList.remove("up");
+  const holeAmount = randAmount(1,3); // call randAmount function to generate an amount of hole.
+  for(let i =0; i < holeAmount; i++) { // for loop for the amount of hole.
+    hole = randomHole(holes); // decide which hole will pop up
+    holeUp[i] = hole; // store the hole in an array for later
+    hole.classList.add("up"); // creating a class list on hole and added up to it. up is in the css.\
+  }
+ // document.getElementsByClassName("professor").game.zIndex = "2";
+  setTimeout(() => { 
+    for(let i =0; i < holeUp.length; i++) {
+      hole = holeUp[i];
+      hole.classList.remove("up"); // remove up so go down
+    }
+   // document.getElementsByClassName("professor").game.zIndex = "2";
     //timeCount = timeUp;
     if (!timeUp) peep(); // When time is not up, it will call the peep() method again.
-    if (timeUp) play = false; // When the time is up, play will turn back to false.
+    if (timeUp) {
+      play = false;
+      document.querySelector('.game').style.cursor = "auto";
+      music.pause();
+      music.currentTime = 0;
+    } // When the time is up, play will turn back to false.
   }, time); // when the time is reach, the professor will pop back in.
 }
 
@@ -56,11 +84,13 @@ when the time is up.
 */
 
 function startGame() {
+  music.play();
   // this code start first.
   if(play == true) { // This if statement is to prevent the click of play button more than once.
     console.log("You are playing the game right now!");
   }
   if(play == false) {
+    document.querySelector('.game').style.cursor = "url(gameImage/hammer_sprite.png), pointer";
     play = true; // change the play to true, when the player press play.
     timeCount = timeAmount / 1000;
     //timer.textContent = timeCount; // display the time.
@@ -80,7 +110,11 @@ the game in order to win, not too necessary.
 */
 function bonk(e) {
   if (!e.isTrusted) return;  
-  score++;
+  hit.play();
+  ouch.play();
+  hit.currentTime = 0;
+  ouch.currentTime = 0;
+  score = score + 10;
   this.parentNode.classList.remove("up");
   scoreBoard.textContent = score;
 }
@@ -91,3 +125,4 @@ function tick() {
 }
 
 professors.forEach((professor) => professor.addEventListener("click", bonk));
+professors2.forEach((professor2) => professor2.addEventListener("click", bonk));
